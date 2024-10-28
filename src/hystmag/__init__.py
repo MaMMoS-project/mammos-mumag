1530d14ebf342fcb65c7d4a84fddffdfb57756ae
Swapneel <swapiitr@gmail.com>
2024-10-28 14:01:44 +0100

    refactor(__init__): clean up print statement and config path

--- src/hystmag/__init__.py
+++ src/hystmag/__init__.py
@@ -2,9 +2,8 @@ import importlib.metadata
 import pathlib
 
 _base_directory = pathlib.Path(__file__).absolute().parent.parent.parent
-print(_base_directory)
 _container_scripts = _base_directory.joinpath("container_scripts")
 _sim_scripts = _base_directory.joinpath("sim_scripts")
 _cache_dir = pathlib.Path.home() / ".cache" / "hystmag"
-_conf_dir = pathlib.Path.home() / ".conf" / "hystmag"
+_conf_dir = pathlib.Path.home() / ".config" / "hystmag"
 __version__ = importlib.metadata.version(__package__)
