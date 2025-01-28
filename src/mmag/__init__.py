cf3b7970bd76f7e3562e82ef12c021f488512689
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-01-28 17:08:13 +0100

    refactor(rename): mammosmag folder is now mmag everywhere

--- src/mmag/__init__.py
+++ src/mmag/__init__.py
@@ -4,6 +4,6 @@ import pathlib
 _base_directory = pathlib.Path(__file__).absolute().parent
 _container_scripts = _base_directory.joinpath("container_scripts")
 _sim_scripts = _base_directory.joinpath("sim_scripts")
-_cache_dir = pathlib.Path.home() / ".cache" / "mammos" / "mammosmag"
-_conf_dir = pathlib.Path.home() / ".config" / "mammos" / "mammosmag"
+_cache_dir = pathlib.Path.home() / ".cache" / "mammos" / "mmag"
+_conf_dir = pathlib.Path.home() / ".config" / "mammos" / "mmag"
 __version__ = importlib.metadata.version(__package__)
