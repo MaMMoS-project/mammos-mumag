d97d443bdcda719bf4d73ccbc6de82674c2401c0
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-02-05 11:31:12 +0100

    refactor: standardize path join
    - Use `/` instead of `.joinpath()`

--- src/mmag/__init__.py
+++ src/mmag/__init__.py
@@ -3,7 +3,7 @@ import pathlib
 
 _base_directory = pathlib.Path(__file__).absolute().parent.parent.parent
 _container_scripts = _base_directory.joinpath("container_scripts")
-_sim_scripts = _base_directory.joinpath("sim_scripts")
+_sim_scripts = _base_directory / 'src/mammosmag/scripts'
 _cache_dir = pathlib.Path.home() / ".cache" / "mammos" / "mmag"
 _conf_dir = pathlib.Path.home() / ".config" / "mammos" / "mmag"
 __version__ = importlib.metadata.version(__package__)
