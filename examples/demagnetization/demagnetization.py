192f6d2bef5422ce846e790f3fa1f3dce76cf731
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-02-05 12:07:07 +0100

    feat(examples): Add demagnetization example

---
+++ examples/demagnetization/demagnetization.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from mammosmag.loop import Loop
+
+here = Path(__file__).absolute().parent
+loop = Loop('cube', outdir=here/'out')
+loop.read_mesh(here/'cube.fly')
+loop.read_params(here/'cube.p2')
+loop.read_materials(here/'cube.krn')
+loop.run()
