f9fcf21fca3e7b9138a7008e2ff646dcb05623b1
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-03-05 17:58:33 +0100

    fix: Fix circular imports

--- src/mammos_mmag/__init__.py
+++ src/mammos_mmag/__init__.py
@@ -6,17 +6,8 @@ MaMMoS micromagnetic simulation software.
 import importlib.metadata
 import pathlib
 
-from .materials import Materials
-from .parameters import Parameters
-from .simulation import Simulation
 
 
 _base_directory = pathlib.Path(__file__).absolute().parent
 _conf_dir = pathlib.Path.home() / ".config" / "mammos" / "mmag"
 __version__ = importlib.metadata.version(__package__)
-
-__all__ = [
-    "Materials",
-    "Parameters",
-    "Simulation",
-]
