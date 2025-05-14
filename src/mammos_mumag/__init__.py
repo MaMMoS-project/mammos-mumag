f4eefa16c97b88812aa6dc932af42401d6482834
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-05-14 19:47:19 +0200

    fix: fix modules

--- src/mammos_mumag/__init__.py
+++ src/mammos_mumag/__init__.py
@@ -7,6 +7,11 @@ import importlib.metadata
 import pathlib
 import shutil
 
+from mammos_mumag.standard_problem import hystloop
+
+__all__ = [
+    "hystloop",
+]
 
 _run_escript_bin = shutil.which("run-escript")
 _scripts_directory = pathlib.Path(__file__).parent.resolve() / "scripts"
