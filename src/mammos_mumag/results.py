44dad1d1cb1e0f42e51ccd4276bb6a171ce72151
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-05-21 14:01:56 +0200

    feat: add get_linearized_segment

--- src/mammos_mumag/results.py
+++ src/mammos_mumag/results.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import pathlib
 import pyvista as pv
+from scipy.optimize import minimize
 from typing import NamedTuple
 from dataclasses import dataclass
 
@@ -82,3 +83,66 @@ class ResultsLoop:
             ),
         )
         return extrprops
+
+    def get_linearized_segment(self):
+        """Evaluate linearized segment."""
+        h = 0.5  # threshold_training
+        mar = 0.05  # margin_to_line
+        m0 = 1.0  # m_guess
+        i0 = 0  # index_adjustment
+        try:
+            upper_index = i0 + np.argmin(np.abs(self.dataframe["polarisation"] - h))
+            hh_u = self.dataframe["mu0_Hext"].iloc[upper_index]
+            df = self.dataframe[self.dataframe["mu0_Hext"] < hh_u]
+
+            lower_index = i0 + np.argmin(np.abs(self.dataframe["polarisation"] >= 0))
+            hh_l = self.dataframe["mu0_Hext"].iloc[lower_index]
+            df = df[df["mu0_Hext"] >= hh_l]
+        except Exception as e:
+            print(f"[ERROR]: Exception: {e}")
+            raise ValueError("Failed Extraction")
+
+        if df.shape[0] < 10:
+            print(
+                f"[ERROR]: Less than 10 points in margin [0,{h}] for linear regression (only {df.shape[0]})"
+            )
+            return 0.0
+
+        def line(x, m, b=0.0):
+            return m * x + b
+
+        def penalty_function(m, x, y, b=0.0):
+            return np.sum((y - line(x=x, m=m, b=b)) ** 2)
+
+        try:
+            b = df["polarisation"].iloc[np.argmin(np.abs(df["mu0_Hext"]))]
+            res = minimize(
+                penalty_function,
+                m0,
+                args=(df["mu0_Hext"], df["polarisation"], b),
+            )
+            m_opt = res.x
+            if not res.success:
+                print(f"Optimization did not converge in general: {res.message}")
+                raise ValueError("Failed Linearization")
+            if m_opt > 1000 or m_opt < 0:
+                print(f"[ERROR]: Slope is unreasonable: {res.x}")
+                raise ValueError("Failed Linearization")
+        except Exception as e:
+            print(f"[ERROR]: Something did not work: {e}.")
+            ValueError("Failed Linearization")
+
+        try:
+            margin = (
+                np.abs(
+                    self.dataframe["polarisation"]
+                    - line(self.dataframe["mu0_Hext"], m_opt, b)
+                )
+                < mar
+            )
+            # npo = np.sum(margin)  # number_points_in_margin
+            x_max_lin = np.max(self.dataframe["mu0_Hext"][margin])
+        except Exception as e:
+            print(f"[ERROR]: Failed x_max_lin extraction: {e}.")
+
+        return x_max_lin
