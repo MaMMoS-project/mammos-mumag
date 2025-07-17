7c1656dd499a6e1eddec5b9c981d3490e12ad9b0
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-07-17 22:14:17 +0200

    feat: allow arrays and lists of entities
    Purpose: multigrain

--- src/mammos_mumag/hysteresis.py
+++ src/mammos_mumag/hysteresis.py
@@ -15,7 +15,7 @@ import pyvista as pv
 from pydantic import ConfigDict
 from pydantic.dataclasses import dataclass
 
-from mammos_mumag.materials import Materials
+from mammos_mumag.materials import MaterialDomain, Materials
 from mammos_mumag.parameters import Parameters
 from mammos_mumag.simulation import Simulation
 
@@ -25,9 +25,11 @@ if TYPE_CHECKING:
 
 
 def run(
-    Ms: float | u.Quantity | me.Entity,
-    A: float | u.Quantity | me.Entity,
-    K1: float | u.Quantity | me.Entity,
+    Ms: float | u.Quantity | me.Entity | list[float | u.Quantity | me.Entity],
+    A: float | u.Quantity | me.Entity | list[float | u.Quantity | me.Entity],
+    K1: float | u.Quantity | me.Entity | list[float | u.Quantity | me.Entity],
+    theta: float | u.Quantity | me.Entity | list[float | u.Quantity | me.Entity],
+    phi: float | u.Quantity | me.Entity | list[float | u.Quantity | me.Entity],
     mesh_filepath: pathlib.Path,
     hstart: float | u.Quantity,
     hfinal: float | u.Quantity,
@@ -53,46 +55,35 @@ def run(
        Result object.
 
     """
+    Ms = _listify_material_parameter("Ms", Ms, "SpontaneousMagnetization")
+    A = _listify_material_parameter("A", A, "ExchangeStiffnessConstant")
+    K1 = _listify_material_parameter("K1", K1, "UniaxialAnisotropyConstant")
+    theta = _listify_material_parameter("theta", theta, "Angle")
+    phi = _listify_material_parameter("theta", theta, "Angle")
+
+    if len({len(e) for e in [Ms, A, K1, theta, phi]}) != 1:
+        print(theta)
+        raise ValueError("All material parameters must have the same length.")
+
     if hstep is None:
         hstep = (hfinal - hstart) / hnsteps
 
-    if not isinstance(A, u.Quantity) or A.unit != u.J / u.m:
-        A = me.A(A, unit=u.J / u.m)
-    if not isinstance(K1, u.Quantity) or K1.unit != u.J / u.m**3:
-        K1 = me.Ku(K1, unit=u.J / u.m**3)
-    if not isinstance(Ms, u.Quantity) or Ms.unit != u.A / u.m:
-        Ms = me.Ms(Ms, unit=u.A / u.m)
+    materials = Materials(
+        domains=[
+            MaterialDomain(
+                theta=theta_i,
+                phi=phi_i,
+                K1=me.Ku(K1_i),
+                Ms=me.Ms(Ms_i),
+                A=me.A(A_i),
+            )
+            for Ms_i, A_i, K1_i, theta_i, phi_i in zip(Ms, A, K1, theta, phi, strict=True)
+        ] + [MaterialDomain()] * 2 # empty loops for air and shell regions
+    )
 
     sim = Simulation(
         mesh_filepath=mesh_filepath,
-        materials=Materials(
-            domains=[
-                {
-                    "theta": 0,
-                    "phi": 0.0,
-                    "K1": K1,
-                    "K2": me.Ku(0),
-                    "Ms": Ms,
-                    "A": A,
-                },
-                {
-                    "theta": 0.0,
-                    "phi": 0.0,
-                    "K1": me.Ku(0),
-                    "K2": me.Ku(0),
-                    "Ms": me.Ms(0),
-                    "A": me.A(0),
-                },
-                {
-                    "theta": 0.0,
-                    "phi": 0.0,
-                    "K1": me.Ku(0),
-                    "K2": me.Ku(0),
-                    "Ms": me.Ms(0),
-                    "A": me.A(0),
-                },
-            ],
-        ),
+        materials=materials,
         parameters=Parameters(
             size=1.0e-9,
             scale=0,
@@ -139,6 +130,18 @@ def run(
         },
         configuration_type=df["configuration_type"].to_numpy(),
     )
+
+
+def _listify_material_parameter(name, parameter, label):
+    if isinstance(parameter, list):
+        if isinstance(parameter, me.Entity) and not all([p.ontology_label == label for p in parameter]):
+                raise ValueError(f"All items of {name} must be {label} entities")
+    else:
+        if isinstance(parameter, me.Entity) and parameter.q.size > 1:
+            parameter = [me.Entity(label, p) for p in parameter.q]
+        if isinstance(parameter, np.ndarray) and parameter.size > 1:
+            parameter = [me.Entity(label, p) for p in parameter]
+    return parameter
 
 
 @dataclass(config=ConfigDict(arbitrary_types_allowed=True, frozen=True))
