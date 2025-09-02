d85c4d5cdc941b32fd7e1ef92b5b44fb351ace70
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-09-02 17:27:46 +0200

    allow retries when downloading mesh

--- tests/test_meshes.py
+++ tests/test_meshes.py
@@ -1,11 +1,8 @@
 """Test mesh module."""
 
-import filecmp
-
 import pytest
-import requests
 
-from mammos_mumag.mesh import Mesh, find_mesh, get_mesh_json
+from mammos_mumag.mesh import Mesh, find_mesh
 
 
 def test_mesh_no_matches():
@@ -30,21 +27,6 @@ def test_mesh_wrong_format():
 
 
 @pytest.mark.parametrize("mesh_name", find_mesh())
-def test_mesh_get_all_meshes(mesh_name):
-    """Test that all meshes are reachable."""
-    zenodo_url = get_mesh_json()["metadata"]["zenodo_url"]
-    mesh_url = f"{zenodo_url}/files/{mesh_name}.fly"
-    res = requests.get(mesh_url)
-    assert res.status_code == 200
-
-
-def test_mesh_write(tmp_path):
-    """Test write method."""
-    mesh = Mesh("cube20_singlegrain_msize1")
-    mesh.write(tmp_path / "mesh.fly")
-    assert (tmp_path / "mesh.fly").is_file()
-    zenodo_url = get_mesh_json()["metadata"]["zenodo_url"]
-    mesh_url = f"{zenodo_url}/files/{mesh.name}.fly"
-    with open(tmp_path / "temp.fly", "wb") as f:
-        f.write(requests.get(mesh_url).content)
-    assert filecmp.cmp(tmp_path / "mesh.fly", tmp_path / "temp.fly")
+def test_mesh_download_all_meshes(mesh_name, tmp_path):
+    """Test that all meshes are downloadable."""
+    Mesh(mesh_name).write(tmp_path / f"{mesh_name}.fly")
