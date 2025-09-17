688a8c0795fddda6e1c2e15b79cd706ed8e74e75
Santa Pile <santapile@Santas-MacBook-Air.local>
2025-09-17 16:53:26 +0200

    added temporary solution for integer cell tags from meshio object

--- src/mammos_mumag/scripts_scikit_fem/mesh.py
+++ src/mammos_mumag/scripts_scikit_fem/mesh.py
@@ -4,14 +4,25 @@
 import sys
 
 #before: from esys.finley import ReadMesh
-import skfem
+from skfem.mesh import Mesh as SkfemMesh
+import meshio
+import numpy as np
+from skfem.io import from_meshio
 
 
 class Mesh:
     def __init__(self, name):
         # print("read mesh from " + name + ".fly")
         #before: ReadMesh(name + ".fly")
-        self._domain = skfem.mesh.Mesh.load(name + ".med")
+        mm = meshio.read(name + ".med")
+        mm.cell_sets = {
+            subdomain[0]: [
+                np.where(tag == idx.item())[0]
+                for tag in mm.cell_data["cell_tags"]
+            ]
+            for idx, subdomain in mm.cell_tags.items()
+        }
+        self._domain = from_meshio(mm)
 
     def getDomain(self):
         return self._domain
