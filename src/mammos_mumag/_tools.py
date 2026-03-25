79e81664c8ae45c38ecceda5787f58e41b865f18
Andrea Petrocchi <andreapetro@protonmail.com>
2026-03-25 11:06:18 +0100

    refactor tools module
    - hide from public API
    - remove functions to simplify code

--- src/mammos_mumag/_tools.py
+++ src/mammos_mumag/_tools.py
@@ -1,43 +1,8 @@
 """Tool functions."""
 
-import pathlib
 from textwrap import dedent
 
 import mammos_mumag
-
-
-def check_path(fname: str | pathlib.Path) -> pathlib.Path:
-    """Check that file exists.
-
-    Args:
-        fname: File path.
-
-    Returns:
-        File path.
-
-    Raises:
-        FileNotFoundError: File not found.
-
-    """
-    path = pathlib.Path(fname).resolve()
-    if not path.is_file():
-        raise FileNotFoundError("File not found.")
-    return path
-
-
-def check_dir(outdir: str | pathlib.Path) -> pathlib.Path:
-    """Check that directory exists.
-
-    Args:
-        outdir: Directory path.
-
-    Returns:
-        Checked directory path.
-
-    """
-    outdir = pathlib.Path(outdir)
-    outdir.mkdir(exist_ok=True, parents=True)
-    return outdir
 
 
 def check_esys_escript() -> None:
